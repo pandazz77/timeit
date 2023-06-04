37944aded4c9135ca28e9eb1505fc69417e55006
pandazz77 <kalin1538inn@gmail.com>
2023-06-04 22:09:43 +0300

    refactor

--- timeit.c
+++ timeit.c
@@ -6,7 +6,8 @@ struct timeval tv1,tv2,dtv;
 struct timezone tz;
 
 void time_start() { gettimeofday(&tv1, &tz); }
-long time_stop(){ gettimeofday(&tv2, &tz);
+long time_stop(){
+  gettimeofday(&tv2, &tz);
   dtv.tv_sec= tv2.tv_sec -tv1.tv_sec;
   dtv.tv_usec=tv2.tv_usec-tv1.tv_usec;
   if(dtv.tv_usec<0) { dtv.tv_sec--; dtv.tv_usec+=1000000; }
